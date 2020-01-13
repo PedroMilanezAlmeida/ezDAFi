bda9ddd6c1bc794f56ad63ffc4eff6e54cee1e9c
PedroMilanezAlmeida <pedromilanez@gmail.com>
2020-01-13 16:32:34 -0300

    Update README

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# DAFi
+# ez-DAFi
 Have you ever wanted to bridge automated clustering techniques and 
 human-driven gating strategies? Now you can, with the new DAFi
 plugin for FlowJo!
@@ -20,13 +20,13 @@ please see: [ https://doi.org/10.1002/cyto.a.23371]( https://doi.org/10.1002/cyt
 Under the hood, the FlowJo plugin extracts your manual gating
 strategy to set the boundaries of regions of interest in your
 parameter hyperspace. Then, it gates on learned cluster centroids
-sitting in these regions of interest and finds all cells that are
-nearest neighbors to centroids of interest. Finally, the plugin
+located in the regions of interest and finds all cells whose
+nearest neighbors are centroids of interest. Finally, the plugin
 automatically gates each cell according to their updated
 identities, returning them to your FlowJo workspace.
 
-Give it try! Just download the __DAFi.jar__ file above or find it
-on [FlowJo's Plugin page](https://www.flowjo.com/exchange/#/) and
-copy it to your FlowJo plugins folder!
+Give it try! Just download the __DAFi.jar__ file above (or find it
+on [FlowJo's Plugin page](https://www.flowjo.com/exchange/#/)) and
+copy the file to your FlowJo plugins folder!
   
   
--- src/com/flowjo/plugin/DAFi/DAFi.java
+++ src/com/flowjo/plugin/DAFi/DAFi.java
@@ -1,6 +1,27 @@
-////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2020 Pedro Milanez-Almeida, Ph.D., NIAID/NIH
+//
+// License
+// The software is distributed under the terms of the
+// Artistic License 2.0
+// http://www.r-project.org/Licenses/Artistic-2.0
+//
+// Disclaimer
+// This software and documentation come with no warranties of any kind.
+// This software is provided "as is" and any express or implied
+// warranties, including, but not limited to, the implied warranties of
+// merchantability and fitness for a particular purpose are disclaimed.
+// In no event shall the  copyright holder be liable for any direct,
+// indirect, incidental, special, exemplary, or consequential damages
+// (including but not limited to, procurement of substitute goods or
+// services; loss of use, data or profits; or business interruption)
+// however caused and on any theory of liability, whether in contract,
+// strict liability, or tort arising in any way out of the use of this
+// software.
+//////////////////////////////////////////////////////////////////////////////
 // Based on the FlowSOM plugin
 //////////////////////////////////////////////////////////////////////////////
+
 
 package com.flowjo.plugin.DAFi;
 
